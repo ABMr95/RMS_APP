1c404b3bcc7e40184f1e8843bc0e94239c97df64
Your Name <you@example.com>
2018-03-11 21:14:55 +0300

    +added a logout button

--- src/components/NB.js
+++ src/components/NB.js
@@ -16,14 +16,25 @@ export default class NB extends Component {
                 </BS.Navbar.Header>
                 <BS.Navbar.Collapse>
                     <BS.Nav>
+                        <BS.NavItem eventKey={6} href="#">My Order</BS.NavItem>
                         <BS.NavItem eventKey={<Meals.All />} href="#">Meals</BS.NavItem>
+
                         <BS.NavDropdown eventKey={3} title="Search Tables" id="basic-nav-dropdown">
                             <BS.MenuItem eventKey={<Meals.One />} href="#">Search Meals</BS.MenuItem>
+                        </BS.NavDropdown>
+
+                        <BS.NavDropdown eventKey={3} title="Admin" id="basic-nav-dropdown">
+                            <BS.MenuItem eventKey={1} href="#">Customer</BS.MenuItem>
+                            <BS.MenuItem eventKey={1} href="#">Meals</BS.MenuItem>
+
                         </BS.NavDropdown>
                     </BS.Nav>
                     <BS.Nav pullRight>
                         <BS.NavItem eventKey={<Auths.Login />} href="#">Log in</BS.NavItem>
                         <BS.NavItem eventKey={<Auths.Register />} href="#">Register</BS.NavItem>
+                        <BS.NavItem eventKey={<Auths.Login />} onClick={() => { sessionStorage.removeItem('token') }} >
+                            Logout
+                        </BS.NavItem>
 
                     </BS.Nav>
                 </BS.Navbar.Collapse>
